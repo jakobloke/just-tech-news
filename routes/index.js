6d952aff669431b3e4c2ca20b9da9b8b54a74989
Jakob <61665831+jakobloke@users.noreply.github.com>
2020-06-04 13:32:12 -0700

    lesson 4

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const commentRoutes = require('./comment-routes');
 
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
 });
+
+router.use('/comments', commentRoutes);
 
 module.exports = router;
